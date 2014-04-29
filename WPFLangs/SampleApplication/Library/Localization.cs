0b3437b71212bcd02d1d574bca506bf5491adbcb
igofed <mail@itornotit.com>
2014-04-29 15:32:45 +0400

    First iteration of #1

--- WPFLangs/SampleApplication/Library/Localization.cs
+++ WPFLangs/SampleApplication/Library/Localization.cs
@@ -1,12 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Markup;
 
 namespace SampleApplication.Library
 {
-    class Localization<TLanguage> where TLanguage : LocalizationLanguage
+    class Localization
     {
+        public static readonly DependencyProperty DesignTimeLanguageProperty = DependencyProperty.RegisterAttached("DesignTimeLanguage", typeof(string), typeof(Localization), new PropertyMetadata(null, OnDesignTimeLanguagePropertyChanged));
+        public static void SetDesignTimeLanguage(UIElement element, string value)
+        {
+            element.SetValue(DesignTimeLanguageProperty, value);
+        }
+        public static string GetDesignTimeLanguage(UIElement element)
+        {
+            return (string)element.GetValue(DesignTimeLanguageProperty);
+        }
+        private static void OnDesignTimeLanguagePropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            if (DesignTimeLanguages == null) DesignTimeLanguages = new Dictionary<DependencyObject, string>();
+
+            var newLanguage = (string)dependencyPropertyChangedEventArgs.NewValue;
+            DesignTimeLanguages[dependencyObject] = newLanguage;
+
+            DesignTimeLanguageChanged(dependencyObject, newLanguage);
+        }
+
+
+        internal static event Action<DependencyObject, string> DesignTimeLanguageChanged = (d, l) => { };
+        internal static Dictionary<DependencyObject, string> DesignTimeLanguages { get; set; }
+    }
+
+    [MarkupExtensionReturnType(typeof(string))]
+    class LocalizableText : MarkupExtension
+    {
+        private readonly string _title;
+        private DependencyObject _targetObject;
+        private DependencyProperty _targetProperty;
+        private DependencyObject _designTimeParent;
+
+        public LocalizableText(string title)
+        {
+            _title = title;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var providerValuetarget = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
+            if (providerValuetarget != null)
+            {
+                _targetObject = (DependencyObject)providerValuetarget.TargetObject;
+                _targetProperty = (DependencyProperty)providerValuetarget.TargetProperty;
+
+                if (DesignerProperties.GetIsInDesignMode(_targetObject))
+                {
+                    if (_designTimeParent == null)
+                    {
+                        Localization.DesignTimeLanguageChanged += OnLocalizationDesignTimeLanguageChanged;
+
+                        _designTimeParent = FindDesignTimeLanguage();
+                    }
+
+                    if (_designTimeParent != null)
+                    {
+                        var language = Localization.DesignTimeLanguages[_designTimeParent];
+                        return language;
+                    }
+
+                    return _title;
+                }
+                else
+                {
+                    return _title;
+                }
+            }
+            return _title;
+        }
+
+        private void OnLocalizationDesignTimeLanguageChanged(DependencyObject designTimeParent, string newLanguage)
+        {
+            if (_designTimeParent == null)
+            {
+                _designTimeParent = FindDesignTimeLanguage();
+            }
+
+            if (designTimeParent == _designTimeParent)
+            {
+                _targetObject.SetValue(_targetProperty, newLanguage);
+            }
+        }
+
+        private DependencyObject FindDesignTimeLanguage()
+        {
+            var currentObject = _targetObject;
+            do
+            {
+                if (Localization.DesignTimeLanguages.ContainsKey(currentObject))
+                {
+                    return currentObject;
+                }
+            } while ((currentObject = LogicalTreeHelper.GetParent(currentObject)) != null);
+
+            return null;
+        }
     }
 }
